0ff0e6a4ef008b344a1aef8c82ad8d28c5cc8790
effiecancode <faitheffie25@gmail.com>
2023-05-10 01:03:27 +0000

    Uppercase func fix

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,5 @@
 def uppercase(s):
 
     for c in s:
-        if ord(c) >= ord('a') and ord(c) <= ord('z'):
-            print("{}".format(chr(ord(c) - ord('a') + ord('A'))), end="")
-        else:
-            print("{}".format(c), end="")
+        print("{}".format(chr(ord(c) - 32)), end="")
     print()
