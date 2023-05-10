d30ef57a277dc0ee79d4d4a8548ab8c6eecfba45
effiecancode <faitheffie25@gmail.com>
2023-05-10 01:58:33 +0000

    tebaphla2

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reverse_alphabet():
-    for i in range(122, 96, -1):    
+    for i in range(122, 96, -1):
         if (i - 96) % 2 == 0:
             print("{:c}".format(i - 32), end="")
         else:
