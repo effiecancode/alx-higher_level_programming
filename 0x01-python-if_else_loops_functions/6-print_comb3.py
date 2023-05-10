023c11aa0590ff5a4db03d6c1281831a2ab96a6b
effiecancode <faitheffie25@gmail.com>
2023-05-10 00:13:32 +0000

    fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i+1, 10):
-        print("{:02d}, {:02d}".format(i, j), end=", ")
+        if i != 0 or j != 1:
+            print("{:02d}, ".format(i*10+j), end="")
 print()
