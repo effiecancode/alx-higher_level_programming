ce221517ee8f349107b58bf92f412db9e10632cf
effiecancode <faitheffie25@gmail.com>
2023-05-10 01:16:45 +0000

    Lst digit fix

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last_digit = number % 10
-    print(last_digit)
-    return last_digit
+    if number < 0:
+        number = number * -1
+    digit = number % 10
+    print("{}".format(num), end="")
+    return digit
