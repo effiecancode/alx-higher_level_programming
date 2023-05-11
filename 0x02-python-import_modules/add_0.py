5f5ac932758b37587c6a1988ff268f57a6944f8d
effiecancode <faitheffie25@gmail.com>
2023-05-11 16:45:28 +0000

    1+2=3

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-def add(a,b):
-    a = 1
-    b = 2
-    return (a+b)
+def add(a, b):
+    """My addition function
 
-if __name__ == "__main__":
-    result = add(a,b)
-    print(result)
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
