2d23e836edb578f111bd2badb2a1f81753f8ca9e
effiecancode <faitheffie25@gmail.com>
2023-05-11 21:14:00 +0000

    args fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-num = len(argv)
-if num < 2:
-    print("{} arguments.".format(num - 1))
+nums = len(argv)
+if nums < 2:
+    print("{} arguments.".format(nums - 1))
 else:
-    if  num == 2:
-        print("{} argument:".format(num - 1))
+    if nums == 2:
+        print("{} argument:".format(nums - 1))
     else:
-        print("{} arguments:".format(num - 1))
-    for i in range(1, num):
+        print("{} arguments:".format(nums - 1))
+    for i in range(1, nums):
         print("{}: {}".format(i, argv[i]))
