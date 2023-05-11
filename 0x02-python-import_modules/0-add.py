84a80e7cea8c7261bd9b74607f0c51f15fbe7973
effiecancode <faitheffie25@gmail.com>
2023-05-11 16:36:30 +0000

    1+2

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from add_0 import add
+
+def result():
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
+
+if __name__ == "__main__":
+    result()
---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add(a,b):
+    a = 1
+    b = 2
+    return (a+b)
+
+if __name__ == "__main__":
+    result = add(a,b)
+    print(result)
