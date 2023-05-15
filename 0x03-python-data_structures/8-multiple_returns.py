4fa6b34d27d13bedc0b83038f00de698940ddefd
effiecancode <faitheffie25@gmail.com>
2023-05-15 15:28:23 +0000

    Multiple returns fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == '' and char0 = sentence[0]:
-      char0 = None
-    return len(sesntence), char0
+    if sentence == ''
+        return 0, None
+    else:
+        return len(sesntence), sentence[0]
