977554a1c34cee90a8c0f6908a4b1b6639ba2dc4
effiecancode <faitheffie25@gmail.com>
2023-05-15 14:14:24 +0000

    Add tupple fix

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     x = tuple_a + (0,0)
-    y = tuble_b + (0,0)
-    return ((x[0] + y[0]) ,(x[1] + y[1]))
+    y = tuple_b + (0,0)
+        return ((x[0] + y[0]) ,(x[1] + y[1]))
