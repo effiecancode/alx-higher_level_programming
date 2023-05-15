c2145e8b8ec5ae2eeb7d592f8d6c57f4e6705ebd
effiecancode <faitheffie25@gmail.com>
2023-05-15 17:26:19 +0000

    Maximum integer fix

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == ():
+    if my_list == []:
         return None
-    max_value = my_list[0]
-    for value in my_list:
-        if value > max_value:
-            max_value = value
-    return max_value
+    max_num = my_list[0]
+    for num in my_list:
+        if num > max_num:
+            max_value = num
+    return max_num
