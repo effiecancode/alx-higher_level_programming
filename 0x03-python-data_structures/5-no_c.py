3a5a318b3b35513efcf75314e27cd014abd81a2f
effiecancode <faitheffie25@gmail.com>
2023-05-15 11:59:41 +0000

    Remove specific chars

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,8 +4,7 @@ def no_c(my_string):
     for letters in my_string:
         new_string.append(letters)
 
-    for letters in len(my_string):
-        if letters != 'c' and != 'C'
+    for letters in range(len(my_string):
+        if letters != 'c' and != 'C':
              new_string.append(letters)
     return ''.join(new_string)
-    
