e7ca48e30375f00a1aae0b37a24be66c2d093807
effiecancode <faitheffie25@gmail.com>
2023-05-15 17:56:33 +0000

    divisible by two in list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -5,6 +5,6 @@ def divisible_by_2(my_list=[]):
         if num % 2 == 0:
             new_list.append(True)
         else:
-           new_list.append(False)
+            new_list.append(False)
     return new_list
         
