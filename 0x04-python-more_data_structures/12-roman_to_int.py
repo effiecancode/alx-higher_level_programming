518fda4bb41dad312bc9bb010096724097435678
effiecancode <faitheffie25@gmail.com>
2023-05-17 23:19:05 +0000

    Roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,5 +4,4 @@ def roman_to_int(roman_string):
         return 0
     romans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     result = sum(romans[c] if romans[c] >= romans.get(p, 0) else -romans[c] for c, p in zip(roman_string, roman_string[1:] + "0"))
-
     return result
