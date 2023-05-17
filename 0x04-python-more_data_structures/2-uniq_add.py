33082a62ed0560ab88a4f64d35282a72c0b2f285
effiecancode <faitheffie25@gmail.com>
2023-05-17 15:33:34 +0000

    sum of unique

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
 
-    uniq_list = list(set(my_list)
+    uniq_list = list(set(my_list))
     result = sum(my_list)
+    print("result: {:d}".format(result)
+
     return result
-        print("result: {:d}".format(result)
