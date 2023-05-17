1346d4db47ed14cea9209b931c1d3169e7f1831e
effiecancode <faitheffie25@gmail.com>
2023-05-17 14:30:20 +0000

    search(iterate), replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
 
-    new_list = My_list[:]
+    new_list = my_list[:]
     for x in range(len(my_list)):
-        if new_list[X] == search
+        if new_list[x] == search:
             new_list[x] = replace
 
     return new_list
