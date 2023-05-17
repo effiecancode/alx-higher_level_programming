be1eb1886644b8be6d42d9773f21dc3ca843b1f5
effiecancode <faitheffie25@gmail.com>
2023-05-17 23:41:23 +0000

    complex delete

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
     for key, value in sorted(a_dictionary.items()):
+        if value == value:
             del a_dictionary[key]
     return a_dictionary
