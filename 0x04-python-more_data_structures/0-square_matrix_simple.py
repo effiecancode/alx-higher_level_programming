3fb6681cac36531a31631cbb805648e23d0ed834
effiecancode <faitheffie25@gmail.com>
2023-05-17 13:20:22 +0000

    unpackage and square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,7 @@ def square_matrix_simple(matrix=[]):
     new_matrix = [[0 for _ in row] for row in matrix]
 
     for x in range(len(matrix)):
-        for y in range(len(matrix)):
+        for y in range(len(matrix[x])):
             new_matrix[x][y] = matrix[x][y] ** 2
 
     return(new_matrix)
