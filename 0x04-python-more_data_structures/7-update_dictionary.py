aadd5a241153c63b1838da65a9e5515702d471d3
effiecancode <faitheffie25@gmail.com>
2023-05-17 20:33:57 +0000

    Update dictionary items

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    updated_dict + dict(a_dictionary)
+    updated_dict = dict(a_dictionary)
     a_dictionary[key] = value
     return updated_dict
 
