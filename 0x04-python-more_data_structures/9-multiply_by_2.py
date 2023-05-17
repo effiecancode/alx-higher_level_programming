bbc0654ce25ff7a7d992887c5ed465d029e92155
effiecancode <faitheffie25@gmail.com>
2023-05-17 21:51:14 +0000

    multiply dict[key] *= 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for key, value in a_dictionary.items():
-        a_dictionary[key] *= 2
-    return a_dictionary
+    new_dict = a_dictionary.copy()
+    for key, value in new_dict.items():
+        new_dict[key] *= 2
+    return new_dict
