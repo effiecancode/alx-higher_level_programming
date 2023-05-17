d190f24698cf04f2cf829eefb2684303970841e0
effiecancode <faitheffie25@gmail.com>
2023-05-17 20:43:31 +0000

    Update dictionary items

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -3,8 +3,3 @@ def update_dictionary(a_dictionary, key, value):
     updated_dict = dict(a_dictionary)
     updated_dict[key] = value
     return updated_dict
-
-def print_sorted_dictionary(a_dictionary):
-    sorted_dict = sorted(a_dictionary.items())
-    for key, value in sorted_dict:
-        print(key, value, sep=": ")
