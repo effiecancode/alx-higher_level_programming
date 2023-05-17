0841a0593146c97c2f79e413d08c29a863803bca
effiecancode <faitheffie25@gmail.com>
2023-05-17 22:29:37 +0000

    retrieve best_score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,8 +3,5 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
 
-    best_score = 0
-    for key in a_dictionary:
-        if a_dictionary[key] > best_score:
-            best_score = a_dictionary[key]
+    best_score =  max(a_dictionary, key=a_dictionary.get)
     return best_score
