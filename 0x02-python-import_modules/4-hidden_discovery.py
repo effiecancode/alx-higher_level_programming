220e9d34b8e2e87d4140d0d0b0378b90a93b2aec
effiecancode <faitheffie25@gmail.com>
2023-05-18 00:01:53 +0000

    bug fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,12 +2,10 @@
 import hidden_4
 
 
+if __name__ == "__main__":
+
     file = dir(hidden_4)
     length = len(file)
-    for i in range(0, length):
-        if file[i] [0:2] != "__":
-            print(file[i])
-
-
-if __name__ == "__main__":
-    print_all()
+    for x in range(0, length):
+        if file[x] [0:2] != "__":
+            print(file[x])
