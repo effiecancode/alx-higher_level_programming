7ba555f39adb7f270a4ea320924f50ac69e5e27f
effiecancode <faitheffie25@gmail.com>
2023-05-29 23:07:47 +0000

    task 0

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,8 +4,8 @@ import sys
 def safe_print_integer_err(value):
     try:
         print('{:d}'.format(value))
+        return True
     except Exception as err:
         print('Exception: {}'.format(error), file=sys.stderr)
-        return False
 
-    return True
+        return False
