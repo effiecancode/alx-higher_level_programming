7ade89b576dae30141a9e76f4f3a25828ae29b0a
effiecancode <faitheffie25@gmail.com>
2023-05-29 21:18:56 +0000

    task 0

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_interger(value):
+
     try:
         print("{:d}".format(value))
-        print()
-        return True
-    except (ValueErroe, TypeError):
+    except (ValueError, TypeError):
         return False
+    else:
+        return True
