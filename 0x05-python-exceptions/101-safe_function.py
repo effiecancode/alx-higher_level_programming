e95fd0eb225a2e149673eaed0932437275c5ac08
effiecancode <faitheffie25@gmail.com>
2023-05-29 23:22:38 +0000

    task final

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+def safe_function(fct, *args):
+
+    try:
+        result = fct(*args)
+        return result
+    except Exception as err:
+        print('Exception: {}'.format(exception), file=sys.stderr)
+
+        return None
