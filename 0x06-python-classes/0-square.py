d8a50d9a6a6a4d3452097366514b88004f03141a
effiecancode <faitheffie25@gmail.com>
2023-06-01 23:50:35 +0000

    Better late, than never!

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""to define a class square """
-
-
-class Square:
-    pass
+Square = __import__('0-square').Square
+my_square = Square()
+print(type(my_square))
+print(my_square.__dict__)
