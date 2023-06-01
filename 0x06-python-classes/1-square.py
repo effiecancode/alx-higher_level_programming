51934b0d8ad895275f5704b834094617b3c1fd3b
effiecancode <faitheffie25@gmail.com>
2023-06-01 23:58:21 +0000

    Better late, than never!

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """class square
+    Attributes:
+        __size (int): size of a side of the square
+    """
+    def __init__(self, size):
+        """Instatiation of a square
+        parameters:
+            size (int): size of a side of the square
+        Returns: None
+        """
+        self.__size = size
