e6d7d26ac31a467dc2d7dc96069485a9c29a3767
effiecancode <faitheffie25@gmail.com>
2023-07-20 03:04:59 +0300

    Join in SQL

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -6,6 +6,6 @@
 -- If the table states already exists, your script should not fail
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+-- USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
 name VARCHAR(256) NOT NULL );
