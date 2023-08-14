ccc08a902f83912fd70c3ae8579054e53ae28601
effiecancode <faitheffie25@gmail.com>
2023-08-15 02:05:24 +0300

    Process.argv😮‍💨

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,14 +2,13 @@
 
 const argscount = process.argv.length - 2;
 
-if (argscount === 0){
-    console.log("No argument");
-} else if (argscount === 1){
-    console.log("Argument found");
+if (argscount === 0) {
+  console.log('No argument');
+} else if (argscount === 1) {
+  console.log('Argument found');
 } else {
-    console.log("Arguments found")
+  console.log('Arguments found');
 }
-
 
 // set argcount to length -2
 // process.argv[0] contains the path to the Node.js executable.
