8739cb4b727d51509f92345cc615ccd352dd3197
effiecancode <faitheffie25@gmail.com>
2023-08-15 02:45:27 +0300

    Print x times🤭

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,17 +1,12 @@
 #!/usr/bin/node
 
-// Check if the number of occurrences argument is provided
 if (process.argv.length < 3) {
   console.log('Missing number of occurrences');
 } else {
-  // Parse the argument as an integer
   const x = parseInt(process.argv[2]);
-
-  // Check if the parsed value is a valid integer
   if (isNaN(x)) {
     console.log('Missing number of occurrences');
   } else {
-    // Loop to print "C is fun" x times
     for (let i = 0; i < x; i++) {
       console.log('C is fun');
     }
