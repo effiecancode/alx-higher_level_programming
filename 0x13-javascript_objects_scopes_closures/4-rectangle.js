a69fa35cd1a28f7a3d3336c83a520b483e64e007
effiecancode <faitheffie25@gmail.com>
2023-08-16 01:44:49 +0300

    Rectangle 4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,30 +1,29 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor (w, h) {
-      if ((w > 0) && (h > 0)) {
-        this.width = w;
-        this.height = h;
-      }
-    }
-
-    print () {
-      for (let i = 0; i < this.height; i++) {
-        let row = '';
-        for (let j = 0; j < this.width; j++) {
-          row += 'X';
-        }
-        console.log(row);
-      }
-    }
-
-    rotate () {
-      [this.width, this.height] = [this.height, this.width];
-    }
-
-    double () {
-      this.width *= 2;
-      this.height *= 2;
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
     }
   }
-  module.exports = Rectangle;
-  
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
