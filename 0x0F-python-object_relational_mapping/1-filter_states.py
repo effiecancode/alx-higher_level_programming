f23a9d72af1fd2f9aabcad8e078df3c6cd7b587e
effiecancode <faitheffie25@gmail.com>
2023-08-21 03:37:16 +0300

    select

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,32 +1,18 @@
 #!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
 
-"""
-Module that connects python script to a database
-"""
+
 if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
-
-    # connect the db using command-line arguments
-    my_db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=argv[1],
-        password=argv[2],
-        db=argv[3]
-    )
-
-    # create the cusror && execute the query
-    my_cursor = my_db.cursor()
-    my_cursor.execute(
-        """SELECT * FROM states WHERE name LIKE
-        BINARY 'N%'ORDER BY states.id ASC
-        """
-        )
-
-    # fetch the data queried
-    my_data = my_cursor.fetchall()
-
-    # iterate to print a tuple
-    for data in my_data:
-        print(data)
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id""")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
+    
