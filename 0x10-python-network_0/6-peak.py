83ca08520fb4c62a4988019d91302ddceef0aa8b
effiecancode <faitheffie25@gmail.com>
2023-09-01 01:08:26 +0300

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,8 @@
 #!/bin/bash/python3
+"""function that finds a peak in a list of unsorted integers"""
 
 def find_peak(list_of_integers):
+    """fuction that finds the peak of a list"""
     left, right = 0, len(list_of_integers) - 1
 
     while left < right:
