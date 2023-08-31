d42aa8aa9579665bd516b16f0bb15350298652cf
effiecancode <faitheffie25@gmail.com>
2023-08-31 21:35:23 +0300

    Body Size f

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# Display the byte size of the HTTP response header for a given URL.
-# Sending a request using curl, piping the response to wc to count bytes🤔
-# curl -s "$1" | wc -c
-
-url=$1
-curl -s "$url" | wc -c
+# Get the byte size of the HTTP response header for a given URL.
+curl -s "$1" | wc -c
