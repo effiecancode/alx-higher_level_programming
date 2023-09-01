aae1f118b8b0de6cd23fdb2092e8652f841443a9
effiecancode <faitheffie25@gmail.com>
2023-09-01 11:43:34 +0300

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,3 +4,21 @@
 
 def find_peak(list_of_integers):
     """Finds a peak in list_of_integers"""
+
+    if list_of_integers is None or list_of_integers == []:
+        return None
+    left = 0
+    right = len(list_of_integers)
+    mid = ((right - left) // 2) + left
+    mid = int(mid)
+    if right == 1:
+        return list_of_integers[0]
+    if right == 2:
+        return max(list_of_integers)
+    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
+            list_of_integers[mid] >= list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[mid:])
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
