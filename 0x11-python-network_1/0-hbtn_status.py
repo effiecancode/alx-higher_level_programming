a48f9fc5bcf43736959710a99fbd80b3d3dc2892
effiecancode <faitheffie25@gmail.com>
2023-09-02 03:40:58 +0300

    hbtn status --fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,9 +6,8 @@ import urllib.request
 
 
 if __name__ == '__main__':
-    import urllib.request
 
-    url = "'https://alx-intranet.hbtn.io/status'"
+    url = "https://alx-intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
         content = response.read()
         print("Body response:")
