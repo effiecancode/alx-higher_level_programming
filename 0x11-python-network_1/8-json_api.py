2e1c5a1a1e18a18767f3b530facaf08dd86738af
effiecancode <faitheffie25@gmail.com>
2023-09-02 05:02:00 +0300

    json api fix

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,7 +7,7 @@ from sys import argv
 if __name__ == '__main__':
 
     url = "http://0.0.0.0:5000/search_user"
-    data = {"q": argv[1][0] if len(argv) == 1 else ""}
+    data = {"q": argv[1][0] if len(argv) > 1 else ""}
     response = requests.post(url, data=data)
 
     try:
