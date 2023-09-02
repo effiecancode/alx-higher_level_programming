bc170b4ab28708febbc626c7273dc04eaa257108
effiecancode <faitheffie25@gmail.com>
2023-09-02 05:11:47 +0300

    github commits py-fix

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,7 +10,8 @@ from sys import argv
 if __name__ == '__main__':
 
     req = requests.get('https://api.github.com/repos/{}/{}/commits'
-                     .format(argv[2], argv[1]))
+                       .format(argv[2], argv[1]))
+
     commits = req.json()
     for commit in commits[:10]:
         print(commit.get('sha'), end=': ')
