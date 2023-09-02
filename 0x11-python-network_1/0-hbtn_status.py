f79991950852914e6946feab68b7bf158b0c99ef
effiecancode <faitheffie25@gmail.com>
2023-09-02 03:36:40 +0300

    hbtn status py fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """Write a Python script that fetches
 https://alx-intranet.hbtn.io/status"""
+
 import urllib.request
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    import urllib.request
 
-    url = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-    with urllib.request.urlopen(url) as response:
-        content = response.read()
-
-    print("Body response:")
-    print("\t- type: {}".format(type(content)))
-    print("\t- utf8 content: {}".format(content.decode("utf-8")))
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
