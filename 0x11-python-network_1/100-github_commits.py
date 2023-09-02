70b4a4450fe033c5329a496a73d942380f00fdf0
effiecancode <faitheffie25@gmail.com>
2023-09-02 05:09:00 +0300

    github commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+list 10 commits (from the most recent to oldest) of the repository and user
+sent in as arguments
+"""
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+
+    req = requests.get('https://api.github.com/repos/{}/{}/commits'
+                     .format(argv[2], argv[1]))
+    commits = req.json()
+    for commit in commits[:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
