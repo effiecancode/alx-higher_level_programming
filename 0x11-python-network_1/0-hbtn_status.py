b15a46a2f45059a3b75f423e80f9a2c5f3a895b7
effiecancode <faitheffie25@gmail.com>
2023-09-02 03:43:23 +0300

    hbtn status .fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,7 +10,9 @@ if __name__ == '__main__':
     url = "https://alx-intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
         content = response.read()
+
+        decoded_content = content.decode('utf-8')
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+        print("\t- utf8 content: {}".format(decoded_content))
