461e27b6fb4a071be1328e74840794e7262438e8
effiecancode <faitheffie25@gmail.com>
2023-09-27 16:17:42 +0300

    readfile mathod in fs module.

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
 const fs = require('fs');
 
+/**
+ * process.argv[2] = filename to be read
+ * process.argv[1] = the file path
+ * process.argv[0] = the Node.js executable
+*/
+
 fs.readFile(process.argv[2], 'utf-8', (error, data) => {
   if (error) {
     console.error(error);
@@ -8,3 +14,7 @@ fs.readFile(process.argv[2], 'utf-8', (error, data) => {
   }
   console.log(data);
 });
+
+/** (error, data) are params for the arrow function
+ * its a callback function to check for errors during file reading
+*/
