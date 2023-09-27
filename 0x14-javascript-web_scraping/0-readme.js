e57e6bcec364219bb4faa2f7bdcf579d539e4b0e
effiecancode <faitheffie25@gmail.com>
2023-09-27 16:10:14 +0300

    readfile mathod in fs module

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', (error, data) => {
   if (error) {
-    console.error(`${error.message}`);
+    console.error(error);
     return;
   }
   console.log(data);
