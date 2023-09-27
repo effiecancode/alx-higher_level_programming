ec305d843cfd2540e7e420772ea369dab2240317
effiecancode <faitheffie25@gmail.com>
2023-09-27 16:32:45 +0300

    StatusCode method

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const request = require('request');
+
+// process.argv[2] = url to request data from
+request(process.argv[2], (error, response) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(`code: ${response.statuscode}`);
+  }
+});
