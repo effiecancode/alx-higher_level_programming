cc45cfe0d754096a84150aeb99d9fcb5046fd82d
effiecancode <faitheffie25@gmail.com>
2023-10-04 21:54:49 +0300

    change header color

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,5 @@
+// Select the <header> element using document.querySelector
+const header = document.querySelector('header');
+
+// Update the color to red (#FF0000)
+header.style.color = '#FF0000';
---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,5 @@
+// Select the <header> element using document.querySelector
+const header = document.querySelector('header');
+
+// Update the text color to red (#FF0000)
+header.style.color = '#FF0000';
